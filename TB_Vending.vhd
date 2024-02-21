deed6045a8c1336b6884086b6d8aaa9577f0a88e
AM205379 <am205379@gmail.com>
2024-02-21 16:14:01 +0330

    Project logic changed

--- TB_Vending.vhd
+++ TB_Vending.vhd
@@ -1,71 +1,67 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.NUMERIC_STD.ALL;
 
 entity Vending_Testbench is
 end entity Vending_Testbench;
 
 architecture Testbench of Vending_Testbench is
 
+    signal CLK, RST                     : std_logic;
+    signal T4_Out, T5_Out, T15_Out      : std_logic;
+    signal Not_Enough, Remaining_Money  : std_logic;
+    signal Coin_in                      : std_logic_vector(3 downto 0);
+    signal Ticket_Select                : std_logic_vector(1 downto 0);
+    signal Submit                       : std_logic;
+
+    constant CLK_PERIOD                 : time := 40 ns;
+
     component Vending
         port (
-            CLK, RST: in std_logic;
-            Coin_in: in std_logic_vector(1 downto 0);
-            Ticket_Select: in std_logic_vector(1 downto 0);
-            Submit: in std_logic
+            CLK, RST                    : in std_logic;
+            Ticket_Select               : in std_logic_vector(1 downto 0);
+            T4_Out, T5_Out, T15_Out     : out std_logic;
+            Coin_in                     : in  std_logic_vector(3 downto 0);
+            Not_Enough, Remaining_Money : out std_logic;
+            Submit                      : in  std_logic
         );
     end component;
 
-    signal CLK_Design, RST, Coin_in, Ticket_Select, Submit : std_logic;
-    signal T4_Out, T5_Out, T15_Out, S500, S1000, Not_Enough, Remaning_Money : std_logic;
+begin
+    UUT : Vending
+        port map (
+            CLK => CLK,
+            RST => RST,
+            Ticket_Select => Ticket_Select,
+            T4_Out => T4_Out,
+            T5_Out => T5_Out,
+            T15_Out => T15_Out,
+            Coin_in => Coin_in,
+            Not_Enough => Not_Enough,
+            Remaining_Money => Remaining_Money,
+            Submit => Submit
+        );
 
-    constant CLK_Period : time := 40 ns;
+    CLK_process : process
     begin
-        Vending_ENT : Vending port map (
-                CLK => CLK_Design,
-                RST => RST,
-                Coin_in => Coin_in,
-                Ticket_Select => Ticket_Select,
-                Submit => Submit
-            );
-
-        CLK_Process: process
-        begin
+        while now < 200 ns loop
             CLK <= '0';
-            wait for 5 ns;
+            wait for CLK_PERIOD/2;
             CLK <= '1';
-            wait for 5 ns;
-        end process;
+            wait for CLK_PERIOD/2;
+        end loop;
+        wait;
+    end process CLK_process;
 
-        Stimulus_Process: process
-        begin
-    
+    Stimulus_Process : process
+    begin
+        -- Test Scenario 1
+        Coin_in <= "0001";     -- Coin value 500
+        Submit <= '0'; 
+	wait for CLK_PERIOD / 2;
+        Ticket_Select <= "11"; -- Select Ticket 4000  
+        Submit <= '1';
 
-            Coin_in <= "01"; -- Coin value 500
-            Ticket_Select <= "01"; -- Select T4
-            Submit <= '0';
-            wait for 10 ns;
-            Submit <= '1';
-            wait for 10 ns;
+        wait;
+    end process Stimulus_Process;
 
-            Coin_in <= "10"; -- Coin value 1000
-            Ticket_Select <= "10"; -- Select T15
-            Submit <= '0';
-            wait for 10 ns;
-            Submit <= '1';
-            wait for 10 ns;
-
-            Coin_in <= "11"; -- Coin value 1500
-            Ticket_Select <= "11"; -- Select T5
-            Submit <= '0';
-            wait for 10 ns;
-            Submit <= '1';
-            wait for 10 ns;
-
-            -- Add more test cases as needed
-
-            wait;
-        end process;
-
-    end Testbench;
+end Testbench;
--- Vending.vhd
+++ Vending.vhd
@@ -1,111 +1,143 @@
-library ieee;
-use ieee.std_logic_1164.all;
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
 
 entity Vending is 
     port (
-        CLK, RST                : in std_logic;
-        Coin_in                 : in std_logic_vector(1 downto 0); -- "01" -> 500T / "10" -> 1000T
-        Ticket_Select           : in std_logic_vector(1 downto 0); -- "01" -> Ticket-500 / "10" -> Ticket-1500 / "11" -> Ticket-4000
-        Submit                  : in std_logic; -- Finish Button 
-        T4_Out, T5_Out, T15_Out : out std_logic
-    );
-end entity Vending;
+        CLK, RST                    : in  std_logic;
+        Ticket_Select               : in  std_logic_vector(1 downto 0); -- "01" -> Ticket-500 / "10" -> Ticket-1500 / "11" -> Ticket-4000
+        T4_Out, T5_Out, T15_Out     : out std_logic;
+        Coin_in                     : in  std_logic_vector(3 downto 0); -- "01" -> 500T / "10" -> 1000T
+        Not_Enough, Remaining_Money : out std_logic;
+        Submit                      : in  std_logic -- Finish Button 
+       
+     );
+end Vending;
 
 architecture Behavior of Vending is
-    type state_Type is (Idle, T4, T5, T15, Error);
+    
+    type State_Type is (Idle, T4, T5, T15, Error);
 
-    signal Current_State, Next_State               : state_Type;
-    signal S500, S1000, Not_Enough, Remaning_Money : std_logic;
+    signal Current_State, Next_State : State_Type;
 
 begin 
-    process (CLK, RST, Coin_in, Ticket_Select, Submit)
-        variable Coin_Counter : integer := 0; -- Counter For Money Pocket
+    process (CLK, RST, Ticket_Select, Submit, Coin_in)
+    
+	variable Coin_Counter : integer range 0 to 1000000 := 0;
 
     begin 
-
         if (RST = '0') then 
             Current_State <= Idle;
 
-        elsif (rising_edge(CLK)) then
+        elsif (clk'event and clk = '1') then
             Current_State <= Next_State;
         end if;
-
-        if (Submit = '0') then
-
-            case Coin_in is
-                when "01" =>
-                    S500 <= '1';
-                when "10" =>
-                    S1000 <= '1';
-                when others =>
-                    S500 <= '0';
-                    S1000 <= '0';
-            end case;
-            
-            if (S500 = '1') then
-                Coin_Counter := Coin_Counter + 500;
-            end if;
-
-            if (S1000 = '1') then
-                Coin_Counter := Coin_Counter + 1000;
-            end if;
-
-            elsif (Submit = '1') then
         
-                case Current_State is 
+        if (Coin_in = "0001") then
+            Coin_Counter := Coin_Counter + 500;
+    
+        elsif (Coin_in = "0010") then
+            Coin_Counter := Coin_Counter + 1000;
 
-                    when Idle =>
-                        if (Ticket_Select = "01") then
-                            Next_State <= T5;
-                        elsif (Ticket_Select = "10") then
-                            Next_State <= T15;
-                        elsif (Ticket_Select = "11") then
-                            Next_State <= T4;
-                        end if;
+        elsif (Coin_in = "0011") then
+            Coin_Counter := Coin_Counter + 1500;
+        
+        elsif (Coin_in = "0100") then
+            Coin_Counter := Coin_Counter + 2000;
+        
+        elsif (Coin_in = "0101") then
+            Coin_Counter := Coin_Counter + 2500;
+        
+        elsif (Coin_in = "0110") then
+            Coin_Counter := Coin_Counter + 3000;
 
-                    when T5 =>
-                        if (Coin_Counter >= 500) then
-                            T5_Out <= '1';
-                            Next_State <= Idle;
-                            Coin_Counter := Coin_Counter - 500;
+        elsif (Coin_in = "0111") then   
+            Coin_Counter := Coin_Counter + 3500;
 
-                        elsif (Coin_Counter < 500) then
-                            Next_State <= Error;
-                        end if;
+        elsif (Coin_in = "1000") then
+            Coin_Counter := Coin_Counter + 4000;
 
-                    when T15 =>
-                        if (Coin_Counter >= 1500) then
-                            T15_Out <= '1';
-                            Next_State <= Idle;
-                            Coin_Counter := Coin_Counter - 1500;
+        elsif (Coin_in = "1001") then
+            Coin_Counter := Coin_Counter + 4500;
 
-                        elsif (Coin_Counter < 1500) then
-                            Next_State <= Error;
-                        end if;
+        elsif (Coin_in = "1101") then
+            Coin_Counter := Coin_Counter + 5000;
+        
+        elsif (Coin_in = "1110") then
+            Coin_Counter := Coin_Counter + 5500;
 
-                    when T4 =>
-                        if (Coin_Counter >= 4000) then
-                            T4_Out <= '1';
-                            Next_State <= Idle;
-                            Coin_Counter := Coin_Counter - 4000;
+        elsif (Coin_in = "1111") then
+            Coin_Counter := Coin_Counter + 6000;
 
-                        elsif (Coin_Counter < 4000) then
-                            Next_State <= Error;
-                        end if;
-
-                    when Error =>
-                        Not_Enough <= '1';
-                        Next_State <= Idle;
-                end case;
-
-            if (Coin_Counter > 0) then
-                Remaning_Money <= '1';
-
-            elsif (Coin_Counter <= 0) then
-                Remaning_Money <= '0';
-            end if;     
         end if;
 
+        if (Submit = '1') then
+
+            case Current_State is 
+
+                when Idle =>
+                    if (Ticket_Select = "01") then
+                        if (Submit = '1') then
+                        Next_State <= T5;
+                        end if;
+                    
+                    elsif (Ticket_Select = "10") then
+                        if (Submit = '1') then
+                        Next_State <= T15;
+                        end if;
+
+                    elsif (Ticket_Select = "11") then
+                        if (Submit = '1') then
+                        Next_State <= T4;
+                        end if;
+                    end if;
+
+                when T5 =>
+                    if (Coin_Counter >= 500) then
+                        T5_Out <= '1';
+                        Coin_Counter := Coin_Counter - 500;
+                        Next_State <= Idle;
+
+                    else
+                        T5_Out <= '0';
+                        Next_State <= Error;
+                    end if;
+
+                when T15 =>
+                    if (Coin_Counter >= 1500) then
+                        T15_Out <= '1';
+                        Coin_Counter := Coin_Counter - 1500;
+                        Next_State <= Idle;
+
+                    else
+                        T15_Out <= '0';
+                        Next_State <= Error;
+                    end if;
+
+                when T4 =>
+                    if (Coin_Counter >= 4000) then
+                        T4_Out <= '1';
+                        Coin_Counter := Coin_Counter - 4000;
+                        Next_State <= Idle;
+                        
+                    else
+                        T4_Out <= '0';
+                        Next_State <= Error;
+                    end if;
+
+                when Error =>
+                    Not_Enough <= '1';
+                    Next_State <= Idle;
+                        
+            end case; 
+        end if;
+
+        if (Coin_Counter /= 0) then
+            Remaining_Money <= '1';
+        else 
+            Remaining_Money <= '0';
+        end if;
+        
     end process;
 
 end architecture Behavior;
